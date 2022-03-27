bcf38c63dec374b8f4e813167860753b4d95607a
AbdulMoeedZahid <abdulmoeedzahid@usf.edu>
2022-03-27 08:55:21 -0400

    Map Push

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,8 +7,11 @@ return (
 	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
 		<Text style={{ color: "#006600", fontSize: 40 }}>Map Screen!</Text>
 		<Ionicons name="md-map-sharp" size={80} color="#006600" />
+		map_function(); 
 	</View>
 );
 };
+
+
 
 export default Map;
