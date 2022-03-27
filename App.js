400cba20503e8251d5e982102dbbd13d6c62aed1
PDuff234 <pduffany@mail.usf.edu>
2022-03-26 20:51:38 -0400

    PDuffs Pushing P

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'; 
+
+
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen";
+import MapScreen from "./screens/MapScreen";
+
+const TabNavigator = createBottomTabNavigator({
+Home: {
+	screen: HomeScreen,
+	navigationOptions: {
+	tabBarLabel: "Home",
+	tabBarOptions: {
+		activeTintColor: "#006600",
+	},
+	tabBarIcon: (tabInfo) => {
+		return (
+		<Ionicons
+			name="md-home"
+			size={24}
+			color={tabInfo.focused ? "#006600" : "#8e8e93"}
+		/>
+		);
+	},
+	},
+},
+User: {
+	screen: UserScreen,
+	navigationOptions: {
+	tabBarLabel: "User",
+	tabBarOptions: {
+		activeTintColor: "#006600",
+	},
+	tabBarIcon: (tabInfo) => {
+		return (
+		<Ionicons
+			name="md-person-circle-outline"
+			size={24}
+			color={tabInfo.focused ? "#006600" : "#8e8e93"}
+		/>
+		);
+	},
+	},
+},
+Map: {
+	screen: MapScreen,
+	navigationOptions: {
+	tabBarLabel: "Map",
+	tabBarOptions: {
+		activeTintColor: "#006600",
+	},
+	tabBarIcon: (tabInfo) => {
+		return (
+		<Ionicons
+			name="md-map-sharp"
+			size={24}
+			color={tabInfo.focused ? "#006600" : "#8e8e93"}
+		/>
+		);
+	},
+	},
+},
+});
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+	<Navigator>
+    <StatusBar style="auto"/>
+	  <HomeScreen/>
+	</Navigator>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
